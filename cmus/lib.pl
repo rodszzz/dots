bc056106cf2705ce1821e6f565bfaec4df0c6b94
Rodrigo <rodrigo@aurora.localdomain>
2021-09-04 18:29:00 -0300

    adding my most useful dots

---
+++ cmus/lib.pl
@@ -0,0 +1,202 @@
+/home/rodrigo/music/CowboyBebop/CowboyBebop-GoodnightJulia_extended.webm
+/home/rodrigo/music/CowboyBebop/CowboyBebop-Memory.webm
+/home/rodrigo/music/CowboyBebop/CowboyBebop_OST3_Blue-Chicken-Bone.mkv
+/home/rodrigo/music/hololive/Battle_Of_Fubuki.mp4
+/home/rodrigo/music/hololive/GawrGura-Reflect.webm
+/home/rodrigo/music/hololive/GETCHA_CalliopeNSuisei.mp4
+/home/rodrigo/music/hololive/GuraAndYamashita-RideOnTime.webm
+/home/rodrigo/music/hololive/HoloBass-RideOnTime.mp4
+/home/rodrigo/music/hololive/HoloBass-UsagiBounce.mp4
+/home/rodrigo/music/hololive/HololiveLofi-LofiTripleVibe.mp4
+/home/rodrigo/music/hololive/PixelShubaDuckDance.mp4
+/home/rodrigo/music/hololive/【作業用BGM】GawrGuraSingingCompilation.mp4
+/home/rodrigo/music/hololive/【歌ってみた_4K】妄想税_癒月ちょこ【オリジナルMV】.mp4
+/home/rodrigo/music/hololive/【歌ってみた】グッバイ宣言_百鬼あやめ_cover.mp4
+/home/rodrigo/music/Home-Odyssey/Home-Come Back Down.webm
+/home/rodrigo/music/Home-Odyssey/Home-Decay.webm
+/home/rodrigo/music/Home-Odyssey/Home-Half Moon-2.webm
+/home/rodrigo/music/Home-Odyssey/Home-Intro.webm
+/home/rodrigo/music/Home-Odyssey/Home-Native.mkv
+/home/rodrigo/music/Home-Odyssey/Home-New Machines.webm
+/home/rodrigo/music/Home-Odyssey/Home-Nights (I Wish I Could Be There).webm
+/home/rodrigo/music/Home-Odyssey/Home-Odyssey.webm
+/home/rodrigo/music/Home-Odyssey/Home-On The Way Out.webm
+/home/rodrigo/music/Home-Odyssey/Home-Oort Cloud.webm
+/home/rodrigo/music/Home-Odyssey/Home-Resonance.webm
+/home/rodrigo/music/Home-Odyssey/Home-Tides.mkv
+/home/rodrigo/music/JPCityPop/4:00_AM.webm
+/home/rodrigo/music/JPCityPop/JunkoOhashi-ILoveYouSo.webm
+/home/rodrigo/music/JPCityPop/JunkoOhashi-TelephoneNumber.webm
+/home/rodrigo/music/JPCityPop/MariyaTakeuchi_竹内まりや_Plastic_Love.webm
+/home/rodrigo/music/JPCityPop/TatsuroYamashita-MagicWays.webm
+/home/rodrigo/music/JPCityPop/TatsuroYamashita-OdoroYoFish.webm
+/home/rodrigo/music/JPCityPop/TatsuroYamashita-RideOnTime.webm
+/home/rodrigo/music/JPCityPop/TatsuroYamashita-Sparkle.webm
+/home/rodrigo/music/JPCityPop/TomokoAran-ImInLove.webm
+/home/rodrigo/music/JPCityPop/真夜中のドア_Stay_With_Me.webm
+/home/rodrigo/music/JPCityPop/黄昏のBayCity.webm
+/home/rodrigo/music/PausaEP_GaeaEP/Pausa EP (2018) _ Gaea EP (2019).m4a
+/home/rodrigo/music/pepesed/SCATOLOVE-O_TIRO.webm
+/home/rodrigo/music/PinkGuy-PinkSeason/Another Earth-TYInkFfAwlA.webm
+/home/rodrigo/music/PinkGuy-PinkSeason/Are You Serious-d5M4U9esJxg.webm
+/home/rodrigo/music/PinkGuy-PinkSeason/Be Inspired-kyAvba2Q11E.webm
+/home/rodrigo/music/PinkGuy-PinkSeason/Club Banger 3000-Ji6-yiOhHvk.webm
+/home/rodrigo/music/PinkGuy-PinkSeason/D I C C W E T T 1-xD9Ife-K4tA.webm
+/home/rodrigo/music/PinkGuy-PinkSeason/Dora the Explora-_BXewUR1oDA.webm
+/home/rodrigo/music/PinkGuy-PinkSeason/Dumplings-GWRCh7EdEvs.webm
+/home/rodrigo/music/PinkGuy-PinkSeason/Flex Like David Icke-FgAhX36WEPw.webm
+/home/rodrigo/music/PinkGuy-PinkSeason/Fried Noodles-ZOLaYl3b-4w.m4a
+/home/rodrigo/music/PinkGuy-PinkSeason/Furr-1Z-vXHNJrXo.webm
+/home/rodrigo/music/PinkGuy-PinkSeason/Gays 4 Donald-uX9HfGwomBk.webm
+/home/rodrigo/music/PinkGuy-PinkSeason/Goofy's Trial-ZvftpofBzZE.m4a
+/home/rodrigo/music/PinkGuy-PinkSeason/Hand on My Gat-0tXXrwxsDGs.m4a
+/home/rodrigo/music/PinkGuy-PinkSeason/Hentai-uW8gnBP3FuU.webm
+/home/rodrigo/music/PinkGuy-PinkSeason/High School Blink193-Gx5qaNHeaJU.m4a
+/home/rodrigo/music/PinkGuy-PinkSeason/Hot Nickel Ball on a Pussy-rxG7NJh6F7w.webm
+/home/rodrigo/music/PinkGuy-PinkSeason/I Do It for My Hood-OwWFYnzjbZ0.m4a
+/home/rodrigo/music/PinkGuy-PinkSeason/I Have a Gun-om9iGHxuCTo.m4a
+/home/rodrigo/music/PinkGuy-PinkSeason/I Will Get a Vasectomy-q3wmG73Ed0c.m4a
+/home/rodrigo/music/PinkGuy-PinkSeason/Meme Machine-XMR1YMHk_4I.webm
+/home/rodrigo/music/PinkGuy-PinkSeason/Nickelodeon Girls-pgVIL0mo6yA.webm
+/home/rodrigo/music/PinkGuy-PinkSeason/PINK GUY - HELP-Ho1LgF8ys-c.webm
+/home/rodrigo/music/PinkGuy-PinkSeason/Pink Life-pO4NLfi-cS4.webm
+/home/rodrigo/music/PinkGuy-PinkSeason/Please Stop Calling Me Gay-IR2vxfuMfvo.webm
+/home/rodrigo/music/PinkGuy-PinkSeason/Please Stop Touching My Willy-ZTcYXfwmcIo.webm
+/home/rodrigo/music/PinkGuy-PinkSeason/Rice Balls-DTQ8bZi8oLk.webm
+/home/rodrigo/music/PinkGuy-PinkSeason/She's So Nice-Em3HL0kFJSU.webm
+/home/rodrigo/music/PinkGuy-PinkSeason/Small Dick-FhugdiahKJM.webm
+/home/rodrigo/music/PinkGuy-PinkSeason/SMD-zrD6LX8rdl4.m4a
+/home/rodrigo/music/PinkGuy-PinkSeason/Stfu-GhJ043zp8hI.webm
+/home/rodrigo/music/PinkGuy-PinkSeason/Uber Pussy-xHCMBjUAO4o.m4a
+/home/rodrigo/music/PinkGuy-PinkSeason/We Fall Again-4noGijzEjLE.m4a
+/home/rodrigo/music/PinkGuy-PinkSeason/セックス大好き-3Beu77WlqmM.m4a
+/home/rodrigo/music/random/AphexTwin-StoneinFocus_field_recorded.mp4
+/home/rodrigo/music/random/CowboyBebop-Rain_FemaleVersion.webm
+/home/rodrigo/music/random/DaftPunk-PeeIsStoredInTheBalls-FullUnreleasedDemo.webm
+/home/rodrigo/music/random/DoomEternal-TheOnlyThingTheyFearIsYou.mp4
+/home/rodrigo/music/random/IEatASs-SmoothmoveSherlock.mp3
+/home/rodrigo/music/random/JohnMurphy-InTheHouse,InaHeartbeat.webm
+/home/rodrigo/music/random/KungFuGeneration-ReRe-_live.mp4
+/home/rodrigo/music/random/MACINTOSHPLUS-リサフランク420現代のコンピュー.webm
+/home/rodrigo/music/random/MarcRebillet-CURE_FOR_DEPRESSION.mp4
+/home/rodrigo/music/random/MarcRebillet-GetUp_Full.mp4
+/home/rodrigo/music/random/McLivinho-NapontadoPé2.webm
+/home/rodrigo/music/random/McTimbu-Palavrasnãobastam[DJBRUNO].webm
+/home/rodrigo/music/random/Miu-Dive.mp4
+/home/rodrigo/music/random/OH_SHIT_IM_FEELING_IT.mp4
+/home/rodrigo/music/random/Poolside_Identity-Chuunibyou×KendrickLamar.mp4
+/home/rodrigo/music/random/Senta-PKdelas.mp3
+/home/rodrigo/music/random/SUR_MON_ILE_DE_REVE-Vasco_and_ChristineBovill.mp4
+/home/rodrigo/music/random/TheFinalCountdown_metalcover-Europe-CalebHyles_JonathanYoung.mp4
+/home/rodrigo/music/random/TimMaia-Medley_byNicolasFresard.mp4
+/home/rodrigo/music/random/Ultrakill_CastleVein.mp4
+/home/rodrigo/music/random/ＶＡＰＩＮＧ　ＩＮ　ＴＨＥ　Ｈ００Ｄ-リサフランク420現代のコンピュー.webm
+/home/rodrigo/music/random/稲葉曇『ラグトレイン』Vo.歌愛ユキ.mp4
+/home/rodrigo/music/russianShit/CREW-EXECUTION_98(BYSLICKKILLA).m4a
+/home/rodrigo/music/Yakuza/Kiruy-chanCantandoBesteira.mp4
+/home/rodrigo/music/Yakuza/Yakuza0-FridayNight_EXTENDED.webm
+/home/rodrigo/music/Yakuza/Yakuza0-FridayNight.mp4
+/home/rodrigo/music/Aests-TheClassics/Aests - The Classics - 01 Drugs.mp3
+/home/rodrigo/music/Aests-TheClassics/Aests - The Classics - 02 FIGHT!.mp3
+/home/rodrigo/music/Aests-TheClassics/Aests - The Classics - 03 Girls.mp3
+/home/rodrigo/music/Aests-TheClassics/Aests - The Classics - 04 Rising Sun.mp3
+/home/rodrigo/music/Aests-TheClassics/Aests - The Classics - 05 Party.mp3
+/home/rodrigo/music/Android52-Remixes/Super_Anime_Groove_3dworld.mp3
+/home/rodrigo/music/Android52-Remixes/Dancing_All_Night.mp3
+/home/rodrigo/music/Android52-Remixes/Romance.mp3
+/home/rodrigo/music/Android52-Remixes/Real_Love.mp3
+/home/rodrigo/music/Flamingosis-Kahunastyle/Flamingosis - Kahunastyle - 01 Stay Humble.mp3
+/home/rodrigo/music/Flamingosis-Kahunastyle/Flamingosis - Kahunastyle - 02 Midnight In Montreal.mp3
+/home/rodrigo/music/Flamingosis-Kahunastyle/Flamingosis - Kahunastyle - 03 Groovin' (feat. Yung Bae).mp3
+/home/rodrigo/music/Flamingosis-Kahunastyle/Flamingosis - Kahunastyle - 04 Future Lounge (feat. A Sol Mechanic).mp3
+/home/rodrigo/music/Flamingosis-Kahunastyle/Flamingosis - Kahunastyle - 05 She Loved Me.mp3
+/home/rodrigo/music/Flamingosis-Kahunastyle/Flamingosis - Kahunastyle - 06 Can It Be.mp3
+/home/rodrigo/music/Flamingosis-Kahunastyle/Flamingosis - Kahunastyle - 07 My Lady.mp3
+/home/rodrigo/music/Flamingosis-Kahunastyle/Flamingosis - Kahunastyle - 08 Cup of Ramen (feat. Go Yama).mp3
+/home/rodrigo/music/Flamingosis-Kahunastyle/Flamingosis - Kahunastyle - 09 Sleeplessness.mp3
+/home/rodrigo/music/Flamingosis-Kahunastyle/Flamingosis - Kahunastyle - 10 Cheer Up.mp3
+/home/rodrigo/music/Flamingosis-Kahunastyle/Flamingosis - Kahunastyle - 11 Helping Hand (feat. Ian Ewing x Harris Cole).mp3
+/home/rodrigo/music/Flamingosis-Kahunastyle/Flamingosis - Kahunastyle - 12 Bushwick Boogie.mp3
+/home/rodrigo/music/Flamingosis-BrightMoments/Flamingosis - Bright Moments - 01 Bright Moments (Feat. The Kount).mp3
+/home/rodrigo/music/Flamingosis-BrightMoments/Flamingosis - Bright Moments - 02 Make me late for breakfast.mp3
+/home/rodrigo/music/Flamingosis-BrightMoments/Flamingosis - Bright Moments - 03 Next to you.mp3
+/home/rodrigo/music/Flamingosis-BrightMoments/Flamingosis - Bright Moments - 04 Flight of The Flamingo.mp3
+/home/rodrigo/music/Flamingosis-BrightMoments/Flamingosis - Bright Moments - 05 feelings of sentimentality due to getting curved.mp3
+/home/rodrigo/music/Flamingosis-BrightMoments/Flamingosis - Bright Moments - 06 Believe in Me.mp3
+/home/rodrigo/music/Flamingosis-BrightMoments/Flamingosis - Bright Moments - 07 Brunch at the bodega.mp3
+/home/rodrigo/music/Flamingosis-BrightMoments/Flamingosis - Bright Moments - 08 All Natural (feat. Tom Ribbons).mp3
+/home/rodrigo/music/Flamingosis-BrightMoments/Flamingosis - Bright Moments - 09 An 8 ball affair.mp3
+/home/rodrigo/music/Flamingosis-BrightMoments/Flamingosis - Bright Moments - 10 Casanova.mp3
+/home/rodrigo/music/Flamingosis-BrightMoments/Flamingosis - Bright Moments - 11 Airplane Mode.mp3
+/home/rodrigo/music/Flamingosis-BrightMoments/Flamingosis - Bright Moments - 12 what went wrong-.mp3
+/home/rodrigo/music/Flamingosis-BrightMoments/Flamingosis - Bright Moments - 13 Sunset Park.mp3
+/home/rodrigo/music/Flamingosis-BrightMoments/Flamingosis - Bright Moments - 14 Ohhh Baby.mp3
+/home/rodrigo/music/Flamingosis-BrightMoments/Flamingosis - Bright Moments - 15 Passing By (feat. Birocratic).mp3
+/home/rodrigo/music/Flamingosis-Daymaker/Flamingosis - Daymaker - 01 First Light.mp3
+/home/rodrigo/music/Flamingosis-Daymaker/Flamingosis - Daymaker - 02 Daybreak.mp3
+/home/rodrigo/music/Flamingosis-Daymaker/Flamingosis - Daymaker - 03 Is It Love - (Remix).mp3
+/home/rodrigo/music/Flamingosis-Daymaker/Flamingosis - Daymaker - 04 Wild Summer.mp3
+/home/rodrigo/music/Flamingosis-Daymaker/Flamingosis - Daymaker - 05 Return To Oasis.mp3
+/home/rodrigo/music/Flamingosis-Daymaker/Flamingosis - Daymaker - 06 Make It Happen.mp3
+/home/rodrigo/music/Flamingosis-Daymaker/Flamingosis - Daymaker - 07 Sunnyvale.mp3
+/home/rodrigo/music/Flamingosis-Daymaker/Flamingosis - Daymaker - 08 Breakdown.mp3
+/home/rodrigo/music/Flamingosis-Daymaker/Flamingosis - Daymaker - 09 Since I Met You.mp3
+/home/rodrigo/music/Flamingosis-Daymaker/Flamingosis - Daymaker - 10 Aurora.mp3
+/home/rodrigo/music/Flamingosis-Daymaker/Flamingosis - Daymaker - 11 Daymaker.mp3
+/home/rodrigo/music/Flamingosis-Daymaker/Flamingosis - Daymaker - 12 Avenue Of The Fireflies.mp3
+/home/rodrigo/music/Flamingosis-Daymaker/Flamingosis - Daymaker - 13 Cosmic Feeling.mp3
+/home/rodrigo/music/Flamingosis-Maniacs!/Flamingosis-Maniacs!-01_So_Far_So_Good.mp3
+/home/rodrigo/music/Flamingosis-Maniacs!/Flamingosis-Maniacs!-02_Future_Love.mp3
+/home/rodrigo/music/Flamingosis-Maniacs!/Flamingosis-Maniacs!-03_Stuntin.mp3
+/home/rodrigo/music/Flamingosis-Maniacs!/Flamingosis-Maniacs!-04_Maniacs(feat._Falcxne).mp3
+/home/rodrigo/music/Flamingosis-Maniacs!/Flamingosis-Maniacs!-05_Favela_Slap.mp3
+/home/rodrigo/music/Flamingosis-Maniacs!/Flamingosis-Maniacs!-06_Double-Jointed.mp3
+/home/rodrigo/music/Flamingosis-Maniacs!/Flamingosis-Maniacs!-07_Slip_Away.mp3
+/home/rodrigo/music/Flamingosis-Maniacs!/Flamingosis-Maniacs!-08_Mantra.mp3
+/home/rodrigo/music/Flamingosis-Maniacs!/Flamingosis-Maniacs!-09_Raise_It_Up!.mp3
+/home/rodrigo/music/Flamingosis-Maniacs!/Flamingosis-Maniacs!-10_Latin_Strut.mp3
+/home/rodrigo/music/Flamingosis-Maniacs!/Flamingosis-Maniacs!-11_Rhythm_Ritual.mp3
+/home/rodrigo/music/Flamingosis-Maniacs!/Flamingosis-Maniacs!-12_West-Coastin.mp3
+/home/rodrigo/music/Flamingosis-Maniacs!/Flamingosis-Maniacs!-13_Nightwalker.mp3
+/home/rodrigo/music/Home-PeeIsStoredInTheBalls/HOME_PEE_IS_STORED_INTHEBALLS_01_Glass.mp3
+/home/rodrigo/music/Home-PeeIsStoredInTheBalls/HOME_PEE_IS_STORED_INTHEBALLS_02_Hanging_Out_On_I7-5.mp3
+/home/rodrigo/music/Home-PeeIsStoredInTheBalls/HOME_PEE_IS_STORED_INTHEBALLS_03_Chicken_Soup.mp3
+/home/rodrigo/music/Home-PeeIsStoredInTheBalls/HOME_PEE_IS_STORED_INTHEBALLS_04_Working_The_Night_Shift.mp3
+/home/rodrigo/music/Home-PeeIsStoredInTheBalls/HOME_PEE_IS_STORED_INTHEBALLS_05_Hurricane_Season_Intro.mp3
+/home/rodrigo/music/Home-PeeIsStoredInTheBalls/HOME_PEE_IS_STORED_INTHEBALLS_06_Handshake.mp3
+/home/rodrigo/music/Home-PeeIsStoredInTheBalls/HOME_PEE_IS_STORED_INTHEBALLS_07_Eye_Doctor.mp3
+/home/rodrigo/music/Home-PeeIsStoredInTheBalls/HOME_PEE_IS_STORED_INTHEBALLS_08_Apathetic.mp3
+/home/rodrigo/music/Home-PeeIsStoredInTheBalls/HOME_PEE_IS_STORED_INTHEBALLS_09_Left_Behind.mp3
+/home/rodrigo/music/Home-PeeIsStoredInTheBalls/HOME_PEE_IS_STORED_INTHEBALLS_10_Joy_Division_Shirt.mp3
+/home/rodrigo/music/Home-PeeIsStoredInTheBalls/HOME_PEE_IS_STORED_INTHEBALLS_11_Completely_Still.mp3
+/home/rodrigo/music/Home-PeeIsStoredInTheBalls/HOME_PEE_IS_STORED_INTHEBALLS_12_Tattoo_Parlor.mp3
+/home/rodrigo/music/Home-PeeIsStoredInTheBalls/HOME_PEE_IS_STORED_INTHEBALLS_13_Still_Wondering.mp3
+/home/rodrigo/music/Home-PeeIsStoredInTheBalls/HOME_PEE_IS_STORED_INTHEBALLS_14_Easter_Island.mp3
+/home/rodrigo/music/Home-PeeIsStoredInTheBalls/HOME_PEE_IS_STORED_INTHEBALLS_15_Goodnight_Bean.mp3
+/home/rodrigo/music/PatriciaTaxxon-TheBestDay/Patricia_Taxxon-The_Best_Day-01_sd_pop.mp3
+/home/rodrigo/music/PatriciaTaxxon-TheBestDay/Patricia_Taxxon-The_Best_Day-02_sd_future.mp3
+/home/rodrigo/music/PatriciaTaxxon-TheBestDay/Patricia_Taxxon-The_Best_Day-03_sd_wasp.mp3
+/home/rodrigo/music/PatriciaTaxxon-TheBestDay/Patricia_Taxxon-The_Best_Day-04_sd_hard.mp3
+/home/rodrigo/music/PatriciaTaxxon-TheBestDay/Patricia_Taxxon-The_Best_Day-05_sd_mustard.mp3
+/home/rodrigo/music/PatriciaTaxxon-TheBestDay/Patricia_Taxxon-The_Best_Day-06_sd_basshours.mp3
+/home/rodrigo/music/PatriciaTaxxon-TheBestDay/Patricia_Taxxon-The_Best_Day-07_sd_grime.mp3
+/home/rodrigo/music/PatriciaTaxxon-TheBestDay/Patricia_Taxxon-The_Best_Day-08_sd_memory.mp3
+/home/rodrigo/music/PatriciaTaxxon-TheBestDay/Patricia_Taxxon-The_Best_Day-09_sd_bbb.mp3
+/home/rodrigo/music/PatriciaTaxxon-TheBestDay/Patricia_Taxxon-The_Best_Day-10_sd_lfo.mp3
+/home/rodrigo/music/PatriciaTaxxon-TheBestDay/Patricia_Taxxon-The_Best_Day-11_sd_house.mp3
+/home/rodrigo/music/PatriciaTaxxon-TheBestDay/Patricia_Taxxon-The_Best_Day-12_sd_plateaus.mp3
+/home/rodrigo/music/PatriciaTaxxon-TheBestDay/Patricia_Taxxon-The_Best_Day-13_sd_yesterday.mp3
+/home/rodrigo/music/PatriciaTaxxon-TheBestDay/Patricia_Taxxon-The_Best_Day-14_sd_darkroom.mp3
+/home/rodrigo/music/PatriciaTaxxon-TheBestDay/Patricia_Taxxon-The_Best_Day-15_sd_lightroom.mp3
+/home/rodrigo/music/PatriciaTaxxon-TheBestDay/Patricia_Taxxon-The_Best_Day-16_sd_heart.mp3
+/home/rodrigo/music/PatriciaTaxxon-TheBestDay/Patricia_Taxxon-The_Best_Day-17_sd_bestday.mp3
+/home/rodrigo/music/TheChemicalBrothers-NoGeography/EveOfDestruction_real.mp3
+/home/rodrigo/music/TheChemicalBrothers-NoGeography/Bango.mp3
+/home/rodrigo/music/TheChemicalBrothers-NoGeography/NoGeography.mp3
+/home/rodrigo/music/TheChemicalBrothers-NoGeography/GotToKeepOn_real.mp3
+/home/rodrigo/music/TheChemicalBrothers-NoGeography/GravityDrops_real.mp3
+/home/rodrigo/music/TheChemicalBrothers-NoGeography/TheUniverseSentMe_real.mp3
+/home/rodrigo/music/TheChemicalBrothers-NoGeography/We'veGotToTry_real.mp3
+/home/rodrigo/music/TheChemicalBrothers-NoGeography/FreeYourself_real.mp3
+/home/rodrigo/music/TheChemicalBrothers-NoGeography/MAH_real.mp3
+/home/rodrigo/music/TheChemicalBrothers-NoGeography/CatchMeI'mFalling_real.mp3
---
+++ cmus/readme.md
@@ -0,0 +1,4 @@
+put the "purple.theme" on /usr/share/cmus if you want to use it
+
+
+to use it open cmus and type :colorscheme purple
